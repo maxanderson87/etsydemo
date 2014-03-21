f2682abb8aa9779d9109280354a5497ea8c0839d
cab <cab@MacBook-Pro-de-cab.local>
2014-03-21 18:00:59 +0100

    oko

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,5 +1,16 @@
 class Listing < ActiveRecord::Base
+  
+  if Rails.env.development?
+    has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "missing.png"
+  
+  else
+    
   has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "missing.png", :storage => :dropbox,
-    :dropbox_credentials => Rails.root.join("config/dropbox.yml")
+    :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
+    :path => ":style/:id_:filename"
+  end
+
    validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
+
 end
